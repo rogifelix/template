2d68f43671de60a61565cf1ecc2711ec94274b5f
Rogerio Felix <rogi.felixthecat@gmail.com>
2021-09-06 09:03:38 -0300

    first commit for the quote generator test project

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log('testing');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+// Get a new quote
+function newQuote() {
+  showLoadingSpinner();
+  // Pick a random quote from the API
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check quote length
+  if (quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  // Check if the author field is blank and replacing it with 'Unknown'
+  if (quote.author == null) {
+    quoteAuthor.textContent = "Unknown";
+  } else {
+    quoteAuthor.textContent = quote.author;
+  }
+
+  quoteText.textContent = quote.text;
+  removeLoadingSpinner();
+}
+
+async function getQuotes() {
+  showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch error here
+    alert("Error");
+  }
+}
+
+// Tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On load
+getQuotes();
